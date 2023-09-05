cf4d7e4b1fee1adfc05e0c7813f19c827fb3d179
bhagwan <bhagwan@disroot.org>
2023-09-05 18:45:27 -0400

    feat: new install script (dedup local repo copies)

--- dots/install.sh
+++ dots/install.sh
@@ -3,32 +3,63 @@ set -eu
 trap 'echo "EXIT detected with exit status $?"' EXIT
 
 # script working directory
-BASEDIR=$(cd "$(dirname "$0")" ; pwd -P)
+BASEDIR=$(cd "$(dirname "$0")"; pwd -P)
+GIT_DIR=$(dirname $(cd "$(git -C ${BASEDIR} rev-parse --git-dir)"; pwd -P))
+DOT_CMD="git -c status.showUntrackedFiles=no --git-dir=${GIT_DIR}/.git -C ${HOME}"
+REPO_URL=$(${DOT_CMD} config --get remote.origin.url)
 
-GIT_CWD=$(git -C ${BASEDIR} rev-parse --show-toplevel)
-GIT_CMD="git -C ${GIT_CWD}"
-DOT_CMD="git -c status.showUntrackedFiles=no --git-dir=${GIT_CWD}/.git -C ${HOME}"
-REPO_URL=$(${GIT_CMD} config --get remote.origin.url)
 
-# pull latest
-echo "[1;32mPulling[0m from [0;33m${REPO_URL}[1;32m...[0m"
-${GIT_CMD} pull --ff-only --progress --rebase=true
+echo "[1;34mBASEDIR:[0;33m  ${BASEDIR}[0m"
+echo "[1;34mGIT_DIR:[0;33m  ${GIT_DIR}/.git[0m"
+echo "[1;34mREPO_URL:[0;33m ${REPO_URL}[0m"
+echo ""
 
-# reset to local HEAD
-echo "[1;32mResetting to [0;33mHEAD[1;32m...[0m"
-${DOT_CMD} reset --hard HEAD
+# new install, reset modules
+if [ ! -d ${GIT_DIR}/.git/modules ]; then
+    git -C ${GIT_DIR} submodule status | \
+        perl -ne '/.\d+\s(.+?)(?=\s|$)/ && print "$1\n"' | \
+        while read path; do
+            echo "[1;32mNew install, deleting [0;33m${path}[0m...[0m"
+            rm -rf ${path}
+        done
+fi
+
+# set current git worktree to $HOME
+echo "[1;32mConfiguring repo[0m [0;34mcore.worktree=[0;33m${HOME}[0m"
+git -C ${BASEDIR} config core.worktree ${HOME}
+
+# configure repo to not display untracked files
+echo "[1;32mConfiguring repo[0m [0;34mstatus.showUntrackedFiles=[0;33mno[0m"
+git -C ${BASEDIR} config --local status.showUntrackedFiles no
+
+if [ ! -L ${GIT_DIR}/.git/.git ]; then
+    # new install, reset to HEAD (expands into $HOME)
+    echo "[1;32mNew install, resetting to [0;33mHEAD[1;32m...[0m"
+    ${DOT_CMD} reset --hard HEAD
+else
+    # pull latest
+    echo "[1;32mPulling[0m from [0;33m${REPO_URL}[1;32m...[0m"
+    ${DOT_CMD} pull --ff-only --progress --rebase=true
+fi
 
 # update submodules
 echo "[1;32mUpdating submodules...[0m"
 ${DOT_CMD} submodule update --init --recursive
 
-# configure repo to not display untracked files
-echo "[1;32mConfiguring repo[0m [0;33mstatus.showUntrackedFiles=no[0m"
-${GIT_CMD} config --local status.showUntrackedFiles no
-
 # setup repo symbolic link at $HOME
-echo "[1;32mSetup repo link [0m [0;33m$HOME/.git[0m -> [0;33m$GIT_CWD/.git[0m"
-ln -fs ${GIT_CWD}/.git ${HOME}/.git
+echo "[1;32mSetup repo link [0m [0;33m$HOME/.git[0m -> [0;33m$GIT_DIR/.git[0m"
+ln -fs ${GIT_DIR}/.git ${HOME}/.git
+
+# Deleting local copies
+if [ ${HOME} != ${GIT_DIR} ]; then
+    echo "[1;32mDeleting local copies in[0;33m ${GIT_DIR} [0m..."
+    ${DOT_CMD} ls-tree --name-only HEAD | while read path; do
+        if [ -e ${GIT_DIR}/${path} ] || [ -L ${GIT_DIR}/${path} ]; then
+            echo "[0;34mrm -rf ${GIT_DIR}/${path}[0m"
+            rm -rf ${GIT_DIR}/${path}
+        fi
+    done
+fi
 
 # Source aliases
 alias dot="${DOT_CMD}"
