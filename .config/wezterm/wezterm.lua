5beeea9637ce484b138ddf85a24ccd948692b376
bhagwan <bhagwan@disroot.org>
2025-01-01 23:29:27 -0800

    feat: ghostty config, updated wezterm config
    tmux: mouse release no longer copies automatically

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,38 +3,43 @@ local config = wezterm.config_builder()
 local act = wezterm.action
 config.font = wezterm.font "Monospace"
 config.font_size = 7.0
+-- https://github.com/wez/wezterm/issues/3774
+config.front_end = "OpenGL"
+config.freetype_load_flags = "NO_HINTING"
+-- config.freetype_load_target = "Light"
+-- config.freetype_render_target = "HorizontalLcd"
 -- Match Alacritty / Kitty default behavior
 config.bold_brightens_ansi_colors = false
 -- Overrides "cursor_{fg|bg|border}"
 -- https://wezfurlong.org/wezterm/config/lua/config/force_reverse_video_cursor.html?h=force_reverse_video_cursor
 config.force_reverse_video_cursor = true
 config.colors = {
-  background = "#323F4E",
-  foreground = "#F8F8F2",
-  selection_bg = "#3D4C5F",
-  selection_fg = "#BD99FF",
+  background = "#1b2738",
+  foreground = "#a6b2c0",
+  selection_bg = "#273951",
+  selection_fg = "#d3dbe5",
   cursor_fg = "black",
-  cursor_bg = "#FFB573",
-  cursor_border = "#FFB573",
+  cursor_bg = "#ddb3ff",
+  cursor_border = "#ddb3ff",
   ansi = {
-    "#3D4C5F", -- black
-    "#F48FB1", -- red
-    "#A1EFD3", -- green
-    "#FFFED5", -- yellow
-    "#92B6F4", -- blue
-    "#FFB2FF", -- magenta
-    "#87DFEB", -- cyan
-    "#F1F1F2", -- white
+    "#011627", -- black
+    "#ff9999", -- red
+    "#85cc95", -- green
+    "#ffd700", -- yellow
+    "#7fb5ff", -- blue
+    "#ddb3ff", -- magenta
+    "#21c7a8", -- cyan
+    "#ffffff", -- white
   },
   brights = {
-    "#56687E", -- black
-    "#EE4F84", -- red
-    "#53E2AE", -- green
-    "#FFFDC2", -- yellow
-    "#6498EF", -- blue
-    "#FF8FFD", -- magenta
-    "#24D1E7", -- cyan
-    "#FFFFFF", -- white
+    "#575656", -- black
+    "#ff9999", -- red
+    "#85cc95", -- green
+    "#ffd700", -- yellow
+    "#7fb5ff", -- blue
+    "#ddb3ff", -- magenta
+    "#85cc95", -- cyan
+    "#ffffff", -- white
   }
 }
 config.initial_rows = 39
@@ -48,7 +53,7 @@ config.window_padding = {
   bottom = 0,
 }
 config.keys = {
-  { key = 's', mods = 'CMD',  action = wezterm.action.PasteFrom "PrimarySelection" },
+  { key = "s", mods = "CMD",  action = wezterm.action.PasteFrom "PrimarySelection" },
   { key = "/", mods = "CTRL", action = wezterm.action { SendString = "\x1f" } },
 }
 local function mouse_bind(direction, streak, button, mods, action)
@@ -65,8 +70,10 @@ config.mouse_bindings = {
   mouse_bind("Up", 1, "Left", "NONE", act.CompleteSelectionOrOpenLinkAtMouseCursor "PrimarySelection"),
   mouse_bind("Up", 2, "Left", "NONE", act.CompleteSelection "PrimarySelection"),
   mouse_bind("Up", 3, "Left", "NONE", act.CompleteSelection "PrimarySelection"),
-  mouse_bind("Up", 1, "Left", "SHIFT", act.CompleteSelectionOrOpenLinkAtMouseCursor "PrimarySelection"),
+  mouse_bind("Up", 1, "Left", "SHIFT",
+    act.CompleteSelectionOrOpenLinkAtMouseCursor "PrimarySelection"),
   mouse_bind("Up", 1, "Left", "ALT", act.CompleteSelection "PrimarySelection"),
-  mouse_bind("Up", 1, "Left", "SHIFT|ALT", act.CompleteSelectionOrOpenLinkAtMouseCursor "PrimarySelection"),
+  mouse_bind("Up", 1, "Left", "SHIFT|ALT",
+    act.CompleteSelectionOrOpenLinkAtMouseCursor "PrimarySelection"),
 }
 return config
